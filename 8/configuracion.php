874aaffe2252f6f820aa56efb4a9a8cc4ac0faf8
jocarsa <info@josevicentecarratala.com>
2022-12-20 17:37:01 +0100

    8

---
+++ 8/configuracion.php
@@ -0,0 +1,15 @@
+<?php
+    
+    $configuracion = [];
+    $bd = new mysqli("localhost", "cms", "cms", "cms");
+    $peticion = "SELECT * FROM configuracion";
+
+    $resultado = $bd->query($peticion);
+
+    while ($fila = $resultado->fetch_assoc()) {
+        $configuracion[$fila['clave']] = $fila['valor'];
+    }
+    include "librerias/funciones.php";
+
+
+?>
---
+++ 8/librerias/funciones.php
@@ -0,0 +1,28 @@
+<?php
+
+    function cargaPagina(){
+        $bd = new mysqli("localhost", "cms", "cms", "cms");
+        $peticion = "SELECT * FROM paginas WHERE Identificador = ".$_GET['id']."";
+        $resultado = $bd->query($peticion);
+        while ($fila = $resultado->fetch_assoc()) {
+            echo '
+                <h3>'.$fila['titulo'].'</h3>
+                <p>'.$fila['contenido'].'</p>
+            ';
+        }
+    }
+    function cargaBlog(){
+        $bd = new mysqli("localhost", "cms", "cms", "cms");
+        $peticion = "SELECT * FROM entradas";
+        $resultado = $bd->query($peticion);
+        while ($fila = $resultado->fetch_assoc()) {
+            echo '
+                <article>
+                    <h3>'.$fila['titulo'].'</h3>
+                    <p>'.$fila['contenido'].'</p>
+                </article>
+            ';
+        }
+    }
+
+?>
